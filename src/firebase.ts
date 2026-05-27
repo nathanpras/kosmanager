5ec093dee9e66c86fc0aaed1385b2442a7ff44b3
Jonathan Prasetyo <chatgptbinus@gmail.com>
2026-05-27 22:34:05 +1000

    feat: configure Firebase SDK v10 with Firestore (placeholder credentials)

---
+++ src/firebase.ts
@@ -0,0 +1,31 @@
+import { initializeApp } from 'firebase/app'
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  doc,
+  addDoc,
+  updateDoc,
+  deleteDoc,
+  setDoc,
+  getDoc,
+  query,
+  orderBy,
+} from 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "kos-manager-93c43.firebaseapp.com",
+  projectId: "kos-manager-93c43",
+  storageBucket: "kos-manager-93c43.firebasestorage.app",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID",
+}
+
+const app = initializeApp(firebaseConfig)
+export const db = getFirestore(app)
+
+export {
+  collection, getDocs, doc, addDoc, updateDoc,
+  deleteDoc, setDoc, getDoc, query, orderBy,
+}
